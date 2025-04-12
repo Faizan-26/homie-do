b71a21805c68e1ed9263fc3e495bf60aaeb0853b
Faizan Tariq <f223858@cfd.nu.edu.pk>
2025-04-12 22:19:42 +0500

    Added react basic app

---
+++ react/homie-doo/src/pages/landing/landing-page.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div className="home-page ">
+        <h1 className="text-3xl">Welcome to the Home Page</h1>
+        <p>This is the home page of our application.</p>
+        </div>
+    );
+    
+}
+
+export default Home;
